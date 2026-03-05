8eeb9da49eac4fa0e280b067b6c8dee884e88b1d
DebugDynamo01 <bhardwajhardik46@gmail.com>
2026-03-05 14:02:04 +0530

    Paameterized constructor example

---
+++ Constructors/Parameterized.java
@@ -0,0 +1,19 @@
+package Constructors;
+class Geeks{
+    String name;
+    int age;
+
+    Geeks(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    void display(){
+        System.out.println("GeekName: " + name + " GeekAge: " + age);
+    }
+
+    public static void main(String[] args){
+        Geeks g1 = new Geeks("Atul", 17);
+        g1.display();
+    }
+}
