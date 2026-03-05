7287e43c7c507a8b332d1f6445b5c23fd62a516c
DebugDynamo01 <bhardwajhardik46@gmail.com>
2026-03-05 13:55:52 +0530

    Default constructor example

---
+++ Constructors/Default.java
@@ -0,0 +1,10 @@
+package Constructors;
+
+class Default {
+    Default(){
+        System.out.println("Default Constructor");
+    }
+    public static void main(String[] args){
+        Default hello = new Default();
+    }
+}
