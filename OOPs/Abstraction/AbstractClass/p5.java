64eb18c0b307145e9c6403197009105784281894
DebugDynamo01 <bhardwajhardik46@gmail.com>
2026-03-07 14:49:47 +0530

    Abstract class ex-5

---
+++ OOPs/Abstraction/AbstractClass/p5.java
@@ -0,0 +1,36 @@
+// Like C++, an abstract class can contain constructors in Java. 
+// And a constructor of an abstract class is called when an 
+// instance of an inherited class is created. It is as shown in 
+// the program below as follows:
+
+package OOPs.Abstraction.AbstractClass;
+
+abstract class Base{
+    //Constructor of class 1
+    Base(){
+        System.out.println("Base constructor called");
+    }
+
+    abstract void fun();    //abstract method
+}
+
+//class 2
+class Derived extends Base{
+    Derived(){  //constructor of class 2
+        System.out.println("Derived Constructor called");
+    }
+    
+    //method of class 2
+    void fun(){
+        System.out.println("Derived func() called");
+    }
+}
+
+// class 3 main class
+class p5 {
+    public static void main(String[] args){
+        //Creating obj of class2 inside main() method
+        Derived d = new Derived();
+        d.fun();
+    }
+}
