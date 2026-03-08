45c7cd97bcfa41217518ef83aeba28d2aed1e132
DebugDynamo01 <bhardwajhardik46@gmail.com>
2026-03-08 18:44:43 +0530

    Polymorphism ex-1

--- OOPs/Polymorphism/p1.java
+++ OOPs/Polymorphism/p1.java
@@ -1,5 +1,20 @@
 package OOPs.Polymorphism;
 
-public class p1 {
-    
+class Person {  // Base class person
+    void role(){    // Method that display the role of a person
+        System.out.println("I am a Person.");
+    }
+}
+
+class Father extends Person{    // Derived class father overrides role method
+    void role(){    // Overridden method to show the role of Father
+        System.out.println("I am a Father.");
+    }
+}
+
+class p1{
+    public static void main(String[] args){
+        Person p = new Father();
+        p.role();
+    }
 }
