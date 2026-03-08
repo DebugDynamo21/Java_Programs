342d2156c00feb34e2b25e142d1470b00311c52a
DebugDynamo01 <bhardwajhardik46@gmail.com>
2026-03-08 18:45:50 +0530

    Polymorphism ex-1

--- OOPs/Polymorphism/p1.java
+++ OOPs/Polymorphism/p1.java
@@ -18,3 +18,9 @@ class p1{
         p.role();
     }
 }
+
+// Explanation: In the above example, the Person class has a method role() that 
+// prints a general message. The Father class overrides role() to print a specific 
+// message. The reference of type Person is used to point to an object of type 
+// Father, demonstrating polymorphism at runtime. The overridden method in Father 
+// is invoked when role() is called.
